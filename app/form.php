f0c63696b13e07ce64835491c7f3dc7d7c17f813
dafamahandika <dafamahandika@gmail.com>
2024-06-20 14:54:45 +0700

    fix get bandara asal dan tujuan

--- app/form.php
+++ app/form.php
@@ -2,7 +2,7 @@
 if(isset($_POST['submit'])){
      $bandara_tujuan = $_POST['bandara_tujuan'];
      $bandara_asal = $_POST['bandara_asal'];
-     echo "<p>Bandara Asal: " . htmlspecialchars($bandaraAsal) . "</p>";
-     echo "<p>Bandara Tujuan: " . htmlspecialchars($bandaraTujuan) . "</p>";
+     echo "<p>Bandara Asal: " . htmlspecialchars($bandara_asal) . "</p>";
+     echo "<p>Bandara Tujuan: " . htmlspecialchars($bandara_tujuan) . "</p>";
 }
 ?>
