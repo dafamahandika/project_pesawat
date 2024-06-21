bda5512b2c745aaee779fc5dbe9ab08244d6377d
dafamahandika <dafamahandika@gmail.com>
2024-06-21 08:48:19 +0700

    delete cdn yang tidak di perlukan

--- views/index.php
+++ views/index.php
@@ -137,11 +137,5 @@
             </tbody>
         </table>
     </div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
